3914499879172612aac82f06d6e89f19113ab07c
Sahil Hasure <sahilhasure08@gmail.com>
2026-06-28 12:02:30 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (35.60%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        // Step 1: Create DP array
+        vector<int> dp(n + 1);
+
+        // Step 2: Base cases
+        dp[0] = 1;
+        dp[1] = 1;
+
+        // Step 3: Fill DP array
+        for(int i = 2; i <= n; i++) {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+
+        // Step 4: Return answer
+        return dp[n];
+    }
+};
