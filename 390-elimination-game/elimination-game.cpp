5ce5be3dfd893bf68e2e70c402d422e14e2ceabf
Sahil_Hasure <sahilhasure08@gmail.com>
2026-07-07 21:09:00 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (6.68%) - LeetSync

---
+++ 390-elimination-game/elimination-game.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int lastRemaining(int n) {
+        int head=1,step=1,remaining=n;
+        bool left=true;
+        while(remaining>1){
+            if(left || remaining%2==1) head+=step;
+            step*=2;
+            remaining/=2;
+            left=!left;
+        }
+        return head;
+    }
+};
